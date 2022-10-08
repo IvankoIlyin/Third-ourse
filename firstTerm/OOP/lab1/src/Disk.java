870509ed9ce39071c71dfdf18eb362d8a42a3f80
Ivanlyin1917 <v.ilyin2003@gmail.com>
2022-10-08 23:12:50 +0300

    .

--- firstTerm/OOP/lab1/src/Disk.java
+++ firstTerm/OOP/lab1/src/Disk.java
@@ -1,37 +1,58 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Disk {
     public List<Song> songs = new ArrayList<Song>();
     private List<Song> byTime = new ArrayList<Song>();
-    public int durationPlayList;
+    private double durationPlayList;
+    private JSONArray disk = new JSONArray();
+    private String path = "./src/disk.json";
 
-    public void findDyration() {
+    public void printPlayList() {
+        for (int i = 0; i < songs.size(); i++) {
+            songs.get(i).printSong(disk);
+        }
         durationPlayList = 0;
         for (int i = 0; i < songs.size(); i++) {
             durationPlayList += songs.get(i).getTime();
         }
-    }
-    public void printPlayList() {
-        for (int i = 0; i < songs.size(); i++) {
-            songs.get(i).printSong();
+        System.out.println("Duration of Playlist: " + durationPlayList);
+        JSONObject duration = new JSONObject();
+        duration.put("Duration:", durationPlayList);
+        disk.add(duration);
+
+        try (FileWriter file = new FileWriter(path)) {
+            file.write(disk.toJSONString());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
-    public void printFindingByTime(){
-        for (int i = 0; i < byTime.size(); i++) {
-            byTime.get(i).printSong();
-        }
-    }
-    public void findByTime(double min, double max) {
+
+    public void printFindingByTime(double min, double max) {
         for (int i = 0; i < songs.size(); i++) {
             if (songs.get(i).getTime() >= min && songs.get(i).getTime() <= max) {
                 byTime.add(songs.get(i));
             }
         }
+        for (int i = 0; i < byTime.size(); i++) {
+            byTime.get(i).printSong(disk);
+        }
+        try (FileWriter file = new FileWriter(path)) {
+            file.write(disk.toJSONString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
-    public void sortByStyle(){
-        songs.sort((a,b) -> a.getStyle().compareTo(b.getStyle()));
+
+    public void sortByStyle() {
+        songs.sort((a, b) -> a.getStyle().compareTo(b.getStyle()));
     }
 
 
