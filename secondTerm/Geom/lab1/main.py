c39519afd5c9fc518774e8e40580a84159345fff
Ivanlyin1917 <52660349+Ivanlyin1917@users.noreply.github.com>
2023-05-19 20:18:29 +0300

    .

--- secondTerm/Geom/lab1/main.py
+++ secondTerm/Geom/lab1/main.py
@@ -1,35 +1,147 @@
-# Локалізація точки на планарному розбитті методом ланцюгів
+import math
 
-import matplotlib.pyplot as plt
 
-def IsInPolygonChainMethod(point, polygon):
-    n = len(polygon)
-    cnt = 0
-    p1x, p1y = polygon[0]
-    for i in range(0, n+1):
-        p2x, p2y = polygon[i % n]
-        if point[1] > min(p1y, p2y):
-            if point[1] <= max(p1y, p2y):
-                if point[0] <= max(p1x, p2x):
-                    if p1y != p2y:
-                        xinters = (point[1]-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
-                    if p1x == p2x or point[0] <= xinters:
-                        cnt += 1
-        p1x, p1y = p2x, p2y
-    return cnt%2!=0
+class Point:
+    def __init__(self, x, y):
+        self.x = float(x)
+        self.y = float(y)
+        self.w_in = 0
+        self.w_out = 0
 
-polygon = [(0, 0), (0, 5), (5, 5), (5, 0)]
+    def __repr__(self):
+        return "(" + str(self.x) + "; " + str(self.y) + ")"
 
-points = [(1, 1), (2, 3), (4, 4), (6, 3), (-1, 1)]
 
-x, y = zip(*(polygon + [polygon[0]]))
-plt.plot(x, y)
+class Edge:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.weight = 0
+        self.to = end.x
+        self.rotation = math.atan2(end.y - start.y, end.x - start.x)
 
-for point in points:
-    if IsInPolygonChainMethod(point, polygon):
-        plt.scatter(point[0], point[1], color='green')
-    else:
-        plt.scatter(point[0], point[1], color='red')
+    def __repr__(self):
+        return str(edges.index(self))
 
-plt.show()
 
+def read_points(file_name):
+    points = []
+    input_array = open(file_name).read().split()
+
+    i = 0
+    while i < len(input_array):
+        points.append(Point(int(input_array[i]), int(input_array[i + 1])))
+        i += 2
+    return points
+
+
+def read_edges(file_name):
+    edgs = []
+    input_array = open(file_name).read().split()
+
+    i = 0
+    while i < len(input_array):
+        edgs.append(Edge(vertices[int(input_array[i])], vertices[int(input_array[i + 1])]))
+        i += 2
+    return edgs
+
+
+def sum_weight(array):
+    result = 0
+    for edge in array:
+        result = result + edge.weight
+    return result
+
+
+def leftmost_edge(array):
+    array = sort_edges(array)
+    return array[0]
+
+
+def leftmost_unused(array):
+    i = 0
+    result = array[0]
+    while i < len(array):
+        if array[i].weight > 0:
+            result = array[i]
+            break
+        i += 1
+    return result
+
+
+def sort_edges(array):
+    return sorted(array, key=lambda edge: edge.rotation, reverse=True)
+
+
+def create_chain(chain_num):
+    current_v = 0
+    while current_v != n - 1:
+        new_in_chain = leftmost_unused(ordered_edges_out[current_v])
+        chains[chain_num].append(new_in_chain)
+        new_in_chain.weight -= 1
+        current_v = vertices.index(new_in_chain.end)
+
+
+def find(point):
+    for p in range(0, num_chains, 1):
+        for e in chains[p]:
+            if e.start.y < point.y < e.end.y:
+                point_vector = Point(point.x - e.start.x, point.y - e.start.y)
+                edge_vector = Point(e.end.x - e.start.x, e.end.y - e.start.y)
+                if math.atan2(point_vector.y, point_vector.x) > math.atan2(edge_vector.y, edge_vector.x):
+                    return "Point is between chains " + str(p - 1) + " , " + str(p)
+    return "Point is not inside graph"
+
+
+vertices = read_points("vertices.txt")
+edges = read_edges("edges.txt")
+
+vertices = sorted(vertices, key=lambda point: point.y)
+
+edges_in = []
+edges_out = []
+
+for v in vertices:
+    edges_in.insert(vertices.index(v), [])
+    edges_out.insert(vertices.index(v), [])
+
+for e in edges:
+    from_idx = vertices.index(e.start)
+    to_idx = vertices.index(e.end)
+    edges_out[from_idx].append(e)
+    edges_in[to_idx].append(e)
+    e.weight = 1
+
+n = len(vertices)
+
+for i in range(1, n - 1):
+    vertices[i].w_in = sum_weight(edges_in[i])
+    vertices[i].w_out = sum_weight(edges_out[i])
+    edges_out[i] = sort_edges(edges_out[i])
+    if vertices[i].w_in > vertices[i].w_out:
+        edges_out[i][0].weight = vertices[i].w_in - vertices[i].w_out + 1
+
+for i in range(n - 1, 1, -1):
+    vertices[i].w_in = sum_weight(edges_in[i])
+    vertices[i].w_out = sum_weight(edges_out[i])
+    edges_in[i] = sort_edges(edges_in[i])
+    if vertices[i].w_out > vertices[i].w_in:
+        edges_in[i][0].weight = vertices[i].w_out - vertices[i].w_in + edges_in[i][0].weight
+
+chains = []
+num_chains = sum_weight(edges_out[0])
+
+ordered_edges_out = [] 
+for v in edges_out:
+    v = sort_edges(v)
+    ordered_edges_out.append(v)
+
+for j in range(num_chains):
+    chains.insert(j, [])
+    create_chain(j)
+
+print(chains)
+
+print(find(Point(5, 10)))
+print(find(Point(7, 7)))
+print(find(Point(2, 2)))
