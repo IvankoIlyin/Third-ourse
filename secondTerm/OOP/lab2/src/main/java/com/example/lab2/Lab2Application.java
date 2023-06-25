fb5c43657dd2455aec7c1a6450a1fed6a776c5f2
Ivanlyin1917 <52660349+Ivanlyin1917@users.noreply.github.com>
2023-06-25 16:32:06 +0300

    .

--- secondTerm/OOP/lab2/src/main/java/com/example/lab2/Lab2Application.java
+++ secondTerm/OOP/lab2/src/main/java/com/example/lab2/Lab2Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Lab2Application {
 
+    //http://localhost:1488/login
     public static void main(String[] args) {
         SpringApplication.run(Lab2Application.class, args);
 
